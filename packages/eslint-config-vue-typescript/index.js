d93be5de4fd0758513d1d7b738527ee9742796e1
neikvon <neikvon@hotmail.com>
2021-01-08 18:13:05 +0800

    fix(eslint-config-vue-typescript): remove

--- packages/eslint-config-vue-typescript/index.js
+++ packages/eslint-config-vue-typescript/index.js
@@ -1,10 +1,32 @@
 module.exports = {
-  extends: ['@fbi-js/typescript', '@fbi-js/vue', '@vue/typescript'],
+  extends: ['@fbi-js/typescript', '@fbi-js/vue'],
+  parserOptions: {
+    parser: require.resolve('@typescript-eslint/parser'),
+    extraFileExtensions: ['.vue'],
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
   rules: {
     indent: 'off',
     '@typescript-eslint/indent': 'off'
   },
   overrides: [
+    {
+      files: ['*.d.ts'],
+      rules: {
+        // The core 'no-unused-vars' rules (in the eslint:recommeded ruleset)
+        // does not work with type definitions
+        'no-unused-vars': 'off'
+      }
+    },
     {
       files: ['*.vue'],
       rules: {
