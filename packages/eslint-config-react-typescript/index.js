e8767bc1d0a337ea902d3cbb16ad556a6bf2aab6
neikvon <neikvon@hotmail.com>
2021-01-08 18:11:21 +0800

    fix(eslint-config-react-typescript): set import/resolver extensions

--- packages/eslint-config-react-typescript/index.js
+++ packages/eslint-config-react-typescript/index.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: ['@fbi-js/react', '@fbi-js/typescript'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  }
 }
