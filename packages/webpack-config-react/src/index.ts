989e51d8ca664581b8768206a8fe1f1eaeb7a35f
neikvon <neikvon@hotmail.com>
2021-01-27 10:55:19 +0800

    fix(webpack-config-react): failed to add plugins

--- packages/webpack-config-react/src/index.ts
+++ packages/webpack-config-react/src/index.ts
@@ -1,22 +1,36 @@
 import { Configuration } from 'webpack'
 import webpackConfigBase, {
+  webpackMerge,
+  resolveOptions,
   ConfigFunctionParams
 } from '@fbi-js/webpack-config-base'
 import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin'
-import resolveOptions from './options'
+import getOptions from './options'
 
 const isDev = process.env.NODE_ENV === 'development'
 
 export default ({
-  options,
-  webpackConfig
+  webpackConfig,
+  options: userOptions
 }: ConfigFunctionParams = {}): Configuration => {
+  const reactOptions = getOptions(userOptions)
+  const options = resolveOptions(reactOptions)
+  let config = webpackConfig
+
   if (isDev) {
-    // this make react fast refresh work, https://github.com/pmmmwh/react-refresh-webpack-plugin#usage
-    webpackConfig?.plugins?.push(new ReactRefreshPlugin())
+    config = webpackMerge.mergeWithRules(options.mergeRules)(
+      {
+        plugins: [
+          // this make react fast refresh work, https://github.com/pmmmwh/react-refresh-webpack-plugin#usage
+          new ReactRefreshPlugin()
+        ]
+      },
+      webpackConfig ?? {}
+    )
   }
+
   return webpackConfigBase({
     options: resolveOptions(options),
-    webpackConfig
+    webpackConfig: config
   })
 }
